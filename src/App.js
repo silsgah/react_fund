df56fa9cbafb1dae278cce7e3c88e956f5f21094
gahsilas <gahsilas@gmail.com>
2024-07-01 20:34:11 +0000

    states added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
 import data from './data.json';
 
 // eslint-disable-next-line
-const Mycomponent = ({pokeman}) => (
+const Mycomponent = ({pokeman, onselect}) => (
   <tr>
     <td>{pokeman.name.english}</td>
     <td>{pokeman.type.join(",")}sample</td>
+    <td>
+      <button 
+      onClick={()=>onselect(pokeman)}
+      >Select</button>
+    </td>
   </tr>
 );
-function App() {
+const PokemanInfo = ({name, base}) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((key)=>(
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{base[key]}</td>
+      </tr>
+      ))}
+    </table>
+  </div>
+)
+export default function App() {
+  const [filter, filterSet] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
   return (
     <div 
       style={{
@@ -18,6 +39,17 @@ function App() {
       width:800
       }}>
        <h1 className='title'>Search</h1>
+       <div
+       style={{
+        display: "grid",
+        gridTemplateColumns: "70% 30%",
+        gridColumnGap: "1rem"
+       }}>
+        <div>
+        <input 
+       onChange={(evt)=>{filterSet(evt.target.value)}}
+       value={filter}
+       />
        <table width="100%">
         <thead>
             <tr>
@@ -26,13 +58,17 @@ function App() {
             </tr>
         </thead>
         <tbody>
-          {data.slice(0,20).map((poke) => (
-            <Mycomponent pokeman={poke} key={poke.id} />
+          {data
+          .filter((datafil)=> datafil.name.english.toLowerCase().includes(filter.toLowerCase()))
+          .slice(0,20).map((poke) => (
+            <Mycomponent pokeman={poke} key={poke.id} onselect={(pokeman) =>setSelectedItem(pokeman)}/>
           ))}
         </tbody>
        </table>
+        </div>
+        {selectedItem  && <PokemanInfo {...selectedItem} />}
+       </div>
+    
     </div>
   );
 }
-
-export default App;
