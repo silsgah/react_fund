f849295a0c09d97eabfe4201e4b2bee0fa9a65b3
gahsilas <gahsilas@gmail.com>
2024-07-02 14:10:00 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
  const [pokeman, Setpokem] = useState([]);
+ const [statefilter, setFilter] = useState("");
  
   useEffect(() =>{
    fetch("http://localhost:3000/data.json")
@@ -16,15 +17,20 @@ function App() {
       paddgingTop: '1rem',
       width:800
     }}>
+      <input  
+      onChange={(evt)=>setFilter((evt.target.value))}
+      value={statefilter}/>
      <table width="100%">
       <thead>
         <tr>
-          <th>Column</th>
-          <th>Column</th>
+          <th>Name</th>
+          <th>Type</th>
         </tr>
       </thead>
       <tbody>
-        {pokeman.map((indiviArray) =>(
+        {pokeman
+        .filter((datfile)=> datfile.name.english.toLowerCase().includes(statefilter.toLowerCase()))
+        .map((indiviArray) =>(
           <tr>
           <td>{indiviArray.name.english}</td>
           <td>{indiviArray.type.join(",")}</td>
