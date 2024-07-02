fb0919c753e72876d72a3cf84ce7334069642bb1
gahsilas <gahsilas@gmail.com>
2024-07-02 13:55:49 +0000

    chnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+//import data from './data/data.json';
 
 function App() {
+ const [pokeman, Setpokem] = useState([]);
+ 
+  useEffect(() =>{
+   fetch("http://localhost:3000/data.json")
+   .then((res) => res.json())
+   .then((data) => Setpokem(data));
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin:"auto",
+      paddgingTop: '1rem',
+      width:800
+    }}>
+     <table width="100%">
+      <thead>
+        <tr>
+          <th>Column</th>
+          <th>Column</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pokeman.map((indiviArray) =>(
+          <tr>
+          <td>{indiviArray.name.english}</td>
+          <td>{indiviArray.type.join(",")}</td>
+        </tr>
+        ))}
+      </tbody>
+     </table>
     </div>
   );
 }
---
+++ src/Profiler.js
@@ -0,0 +1,10 @@
+function Profiler() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Female"
+      />
+    )
+  }
+  
+  export default Profiler;
